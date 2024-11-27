dcceccdd72008dff24c03915cb92d4d3d8f13e31
beatric3 <110557366+beatric3@users.noreply.github.com>
2024-11-27 02:25:36 -0300

    Update menu.js
    ajuste menu

--- menu.js
+++ menu.js
@@ -1,8 +1,6 @@
-
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
-
 const darkMode = document.querySelector('.dark-mode');
 
 menuBtn.addEventListener('click', () => {
@@ -17,65 +15,49 @@ darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-})
-
-
-Orders.forEach(order => {
-    const tr = document.createElement('tr');
-    const trContent = `
-        <td>${order.productName}</td>
-        <td>${order.productNumber}</td>
-        <td>${order.paymentStatus}</td>
-        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
-        <td class="primary">Details</td>
-    `;
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
 });
 
 document.getElementById('contratos').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-contratos');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show'); 
 });
 
 document.getElementById('login').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-login');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('banco-de-horas').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-Banco');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('avaliacao').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-avaliacao');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('cargo').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-cargo');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('folha-de-pagamento').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-folha');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('folha-de-ponto').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-ponto');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('fornecedores').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-fornecedores');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
 
 document.getElementById('funcionarios').addEventListener('click', function() {
     var submenu = document.getElementById('submenu-funcionarios');
-    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+    submenu.classList.toggle('show');
 });
-
-
